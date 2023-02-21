4946a1e7b9a0a3b6cc5518ee9733cfda5a9658c7
EROS <yassine.kader2017fc@gmail.com>
2023-02-21 23:51:46 +0100

    add polygone section

--- main.py
+++ main.py
@@ -9,23 +9,23 @@ def read_qr_code(img):
         value = decode(img)
         data = []
         for barcode in value:
-            data.append({'data':barcode.data.decode("utf-8"), 'type':barcode.type})
+            data.append({'data':barcode.data.decode("utf-8"), 'type':barcode.type,'polygon':barcode.polygon})
+            print(barcode)
+            print("hhhhhhhh")
         return data
     except:
         return
-
+    
 @app.route('/scan_qr', methods=['POST'])
 def scan_qr():
     print(request)
     if 'image' not in request.files:
         return jsonify({'error': 'No image uploaded'}), 400
-
     img_file = request.files['image']
     img = Image.open(img_file.stream)
     results = read_qr_code(img)
     if results == None:
         return jsonify({'error': results}), 404
-
     qr_data = results
     return jsonify({'data': qr_data}), 200
 
