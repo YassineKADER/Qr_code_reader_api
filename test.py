ca9c8b39322bde42d1ecd8ca84c9c7b5d540c8e5
EROS <yassine.kader2017fc@gmail.com>
2023-02-22 00:05:41 +0100

    testing the polygon section

--- test.py
+++ test.py
@@ -1,7 +1,36 @@
 import requests
+import cv2
+import numpy as np
+
 url = 'http://127.0.0.1:5000/scan_qr'
-my_img = {'image': open('./qr-code.png', 'rb')}
+my_img = {'image': open('./cap.png', 'rb')}
 r = requests.post(url, files=my_img)
 
 # convert server response into JSON format.
 print(r.json())
+image = cv2.imread("cap.png")
+
+array = []
+
+for data in r.json()["data"]:
+    array.append(data['polygon'])
+
+print(array)
+
+pts = np.array(array, np.int32)
+
+color = (0,0,255)
+isClosed = True
+thickness = 2
+
+image = cv2.polylines(image, pts,
+                      isClosed, color, thickness)
+ 
+# Displaying the image
+while(1):
+     
+    cv2.imshow('image', image)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+         
+cv2.destroyAllWindows()
